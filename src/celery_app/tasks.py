e13177d367fd7989045087bd44793eea8d79a436
ScottWombat <scottwombat@gmail.com>
2023-09-20 18:50:17 +1000

    tasks

--- src/celery_app/tasks.py
+++ src/celery_app/tasks.py
@@ -1,9 +1,20 @@
 import time
+from time import sleep
+from celery import current_task
 from .worker import celery_app
 
 @celery_app.task
-def test_celery(x, y):
+def add(x, y):
     for i in range(5):
         time.sleep(1)
         print(i)
     return x+y
+
+
+@celery_app.task(acks_late=True)
+def greeting(word: str) -> str:
+    for i in range(1, 11):
+        sleep(1)
+        current_task.update_state(state='PROGRESS',
+                                  meta={'process_percent': i*10})
+    return f"Hello {word}"
--- src/celery_app/worker.py
+++ src/celery_app/worker.py
@@ -3,11 +3,13 @@ import os
 from core.config import config
 
 celery_app = Celery(__name__)
-celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
-   
+celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis_broker:6379")
+celery_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis_broker:6379")
+
 celery_app.conf.task_routes = {
-    "celery_app.tasks.test_celery": "test-queue"}
+    "celery_app.tasks.add": "add-queue",
+    "celery_app.tasks.greeting": "greeting-queue",
+}
 
 # Optional configuration, see the application user guide.
 celery_app.conf.update(
